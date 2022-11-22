db3c49337319dd5b2f20e1a3b4f89aa39528a8a8
Óliver Silva <n2oliver@github.com>
2022-11-22 14:47:50 -0300

    corrige ts

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -62,3 +62,19 @@ import SupportIcon from './icons/IconSupport.vue'
     </ul>
   </WelcomeItem>
 </template>
+
+<script lang="ts">
+VANTA.NET({
+  el: "body",
+  mouseControls: false,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xd7ebe1,
+  backgroundColor: 0xf5f5f5,
+  showDots: false
+})
+</script>
