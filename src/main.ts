f7a13bd11e34a9798c7fa989d0e3c681dfc7880d
Óliver Silva <n2oliver@github.com>
2022-11-22 14:34:55 -0300

    Adiciona fundo animado.

--- src/main.ts
+++ src/main.ts
@@ -9,3 +9,17 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+VANTA.NET({
+  el: "body",
+  mouseControls: false,
+  touchControls: true,
+  gyroControls: true,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0xd7ebe1,
+  backgroundColor: 0xf5f5f5,
+  showDots: false
+})
